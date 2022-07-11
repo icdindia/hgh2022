6122548f2350e0a9953c4cbb37d66e52208e47d7
Alok Joshi <alok@icdindia.com>
2022-07-11 22:55:08 +0530

    export file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
+module.exports = ({
   // pwa: {
   //   dest: "public",
   //   runtimeCaching,
